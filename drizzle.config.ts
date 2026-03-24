8c7bda60716d8bd845c5892a98d0fb763e73966c
deafirefly <deafirefly@gmail.com>
2026-03-24 17:35:57 -0400

    fix: env.production

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	out: './drizzle',
 	dialect: 'sqlite',
 	dbCredentials: {
-		url: 'file:local.db',
+		url: process.env.TURSO_URL ?? 'file:local.db',
+		authToken: process.env.TURSO_AUTH_TOKEN,
 	},
 });
